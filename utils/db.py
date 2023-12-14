9998556cf454078f4e1fd81b0daceba56555c1fa
MSchutt <markus.schuettengruber@gmx.net>
2023-12-14 22:51:44 +0100

    feat: use streamlit secret feature

--- utils/db.py
+++ utils/db.py
@@ -3,16 +3,14 @@ from clickhouse_connect.driver.client import Client
 from clickhouse_connect import common
 import streamlit as st
 
-from constants.database import DB_HOST, DB_PASSWORD, DB_PORT, DB_USERNAME
-
 common.set_setting('autogenerate_session_id', False)  # This should always be set before creating a client
 
 @st.cache_resource
 def get_db() -> Client:
     return clickhouse_connect.create_client(
-        host=DB_HOST,
-        port=DB_PORT,
-        username=DB_USERNAME,
-        password=DB_PASSWORD,
+        host=st.secrets["DB_HOST"],
+        port=st.secrets["DB_PORT"],
+        username=st.secrets["DB_USERNAME"],
+        password=st.secrets["DB_PASSWORD"],
         secure=True
     )
